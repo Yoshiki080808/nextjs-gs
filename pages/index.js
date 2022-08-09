c496ceff80f0d4691db325a31c8e54d61ca607df
Yoshiki080808 <kaneyoshi0729@gmail.com>
2022-08-09 14:23:57 +0900

    test4

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div className='text-sm'>
       <Link href='/blog/3'>
         <a>ブログページNEW（next/link）</a>
       </Link>
       
-      <a href='/hello'>ブログページ（a タグ）</a>
+      {/* <a href='/hello'>ブログページ（a タグ）</a> */}
       <img src='/vercel.svg' />
       <div>Hello World!</div>
       <Image
